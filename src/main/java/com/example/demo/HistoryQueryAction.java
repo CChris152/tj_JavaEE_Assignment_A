144eea3bd563334c28ce982a3900f6ebf0665763
CChris152 <1650306859@qq.com>
2024-09-28 11:06:03 +0800

    first commit

---
+++ src/main/java/com/example/demo/HistoryQueryAction.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class HistoryQueryAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        System.out.println("这是一个插件");
+        System.out.println("这是一个插件");
+    }
+}
